7275686941bf89e5566465c70166ccd8bade9e2f
areif-dev <ajreifsnyder@protonmail.com>
2024-10-22 16:48:08 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,28 @@
-{ rustPlatform, version ? "git", lib }:
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+}:
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   pname = "pta-template-engine";
-  inherit version;
+  version = "0.1.0";
 
-  src = lib.cleanSource ./.;
+  src = fetchFromGitHub {
+    owner = "areif-dev";
+    repo = "pta-template-engine";
+    rev = "4f28ec28143b256da92ec10cb055ec6a0d8b8161";
+    hash = "sha256-0DWlehZhB8hE4JJHjD/e7p+tO6W8JhwSgrMlqs0k4Pg=";
+  };
 
-  cargoLock.lockFile = ./Cargo.lock;
+  cargoHash = "sha256-riLzjP2MtJeyPCYEDW6qI59FGsyR2feXRoeaayjG9go=";
+
+  meta = {
+    homepage = "https://github.com/areif-dev/pta-template-engine";
+    description = "Automate creating complex plaintext accounting journal entries with Jinja2 style templates";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ areif-dev ];
+    mainProgram = "ptatemp";
+    platforms = lib.platforms.linux;
+  };
 }
